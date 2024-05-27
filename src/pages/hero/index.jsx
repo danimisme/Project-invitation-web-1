b8f9d776151c78dfb2b5fe45bfa918e58a0e57fb
danimisme <msubhanr53@gmail.com>
2024-05-27 19:59:47 +0700

    fix scroll and and song

--- src/pages/hero/index.jsx
+++ src/pages/hero/index.jsx
@@ -1,48 +1,55 @@
-import CountDown from '../../components/molekuls/CountDown';
-import { Link } from 'react-router-dom';
+import { autoPlay } from "../../components/molekuls/Audio";
+import CountDown from "../../components/molekuls/CountDown";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
-   const newDate = new Date('June 1, 2024').getTime();
-   const rootElement = document.querySelector(':root');
-   // const navigate = useNavigate();
+  const newDate = new Date("June 1, 2024").getTime();
+  const rootElement = document.querySelector(":root");
+  // const navigate = useNavigate();
 
-   function disableScroll() {
-      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  function disableScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollLeft =
+      window.pageXOffset || document.documentElement.scrollLeft;
 
-      window.onscroll = function () {
-         window.scrollTo(scrollLeft, scrollTop);
-      };
+    window.onscroll = function () {
+      window.scrollTo(scrollLeft, scrollTop);
+    };
 
-      rootElement.style.scrollBehavior = 'auto';
-   }
+    rootElement.style.scrollBehavior = "auto";
+  }
 
-   function enableScroll() {
-      window.onscroll = function () {
-         rootElement.style.scrollBehavior = 'smooth';
-         autoPlay();
-      };
-   }
+  function enableScroll() {
+    window.onscroll = function () {
+      autoPlay();
+    };
 
-   function autoPlay() {
-      const song = document.querySelector('#song');
-      console.log(song);
-      song.play();
-   }
+    rootElement.style.scrollBehavior = "smooth";
+    window.location.href = "#home";
+  }
 
-   disableScroll();
+  //   function autoPlay() {
+  //     const song = document.querySelector("#ssong");
+  //     console.log(song);
+  //     song.play();
+  //   }
 
-   return (
-      <section id='#hero' className='hero w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white'>
-         <main>
-            <h4>Kepada Bapak/Ibu/Saudara/i,</h4>
-            <h1>Dani & Halimah</h1>
-            <p>Akan melangsungkan resepsi pernikahan dalam:</p>
-            <CountDown newDate={newDate} />
-            <Link to='#home' className='btn btn-lg mt-4' onClick={enableScroll}>
-               Lihat undangan
-            </Link>
-         </main>
-      </section>
-   );
+  disableScroll();
+
+  return (
+    <section
+      id="#hero"
+      className="hero w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white"
+    >
+      <main>
+        <h4>Kepada Bapak/Ibu/Saudara/i,</h4>
+        <h1>Dani & Halimah</h1>
+        <p>Akan melangsungkan resepsi pernikahan dalam:</p>
+        <CountDown newDate={newDate} />
+        <a className="btn btn-lg mt-4" onClick={enableScroll}>
+          Lihat undangan
+        </a>
+      </main>
+    </section>
+  );
 }
